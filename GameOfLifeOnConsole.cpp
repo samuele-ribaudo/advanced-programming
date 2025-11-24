33ac8eb6beea3152e0c81ac7adce3f5b24ef88d7
Samuele Ribaudo <samuele.ribaudo@tum.de>
2025-11-24 11:31:35 +0100

    Uploaded Simon's implementation

---
+++ GameOfLifeOnConsole.cpp
@@ -0,0 +1,154 @@
+// use SDL2 or raylib to render the game
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <stdlib.h>
+
+#include <chrono>
+#include <thread>
+
+using std::cin;
+using std::cout;
+using std::string;
+
+class Cell
+{
+public:
+    bool isAlive;
+    bool nextState;
+};
+
+void DrawLine(int l);
+
+int main()
+{
+    srand(time(0));
+
+    int gridSizeX = 80;
+    int gridSizeY = 20;
+
+    // create grid with random alive cells
+    Cell grid[gridSizeX][gridSizeY];
+    for (int y = 0; y < gridSizeY; y++)
+    {
+        for (int x = 0; x < gridSizeX; x++)
+        {
+            grid[x][y].nextState = false;
+
+            int randNum = rand() % 100;
+            if (randNum < 20)
+                grid[x][y].isAlive = true;
+            else
+                grid[x][y].isAlive = false;
+        }
+    }
+
+    // Sim loop
+    int loop = 0;
+    while (loop < 10000)
+    {
+        std::this_thread::sleep_for(std::chrono::milliseconds(50));
+
+        loop++;
+
+        // print grid
+        system("clear");
+        DrawLine(gridSizeX);
+
+        for (int y = 0; y < gridSizeY; y++)
+        {
+            for (int x = 0; x < gridSizeX; x++)
+            {
+                if (grid[x][y].isAlive)
+                {
+                    cout << "█";
+                }
+                else
+                {
+                    cout << " ";
+                }
+            }
+            cout << '\n';
+        }
+        DrawLine(gridSizeX);
+        cout << "Iteration: " << loop << '\n';
+
+        // calculate next state
+        for (int y = 0; y < gridSizeY; y++)
+        {
+            for (int x = 0; x < gridSizeX; x++)
+            {
+                // check wether neighbors are alive
+                int aliveNeighbours = 0;
+                for (int x_off = -1; x_off < 2; x_off++)
+                {
+                    for (int y_off = -1; y_off < 2; y_off++)
+                    {
+                        // ignore itself
+                        if (x_off == 0 && y_off == 0)
+                            continue;
+
+                        int x_neighbour = x + x_off;
+                        int y_neighbour = y + y_off;
+
+                        // check wether the cell is at the edge
+                        // if (x_neighbour < 0 || x_neighbour >= gridSizeX || y_neighbour < 0 || y_neighbour >= gridSizeY)
+                        //     continue;
+
+                        // Loop at edge
+                        if (x_neighbour < 0)
+                            x_neighbour = gridSizeX - 1;
+                        if (x_neighbour >= gridSizeX)
+                            x_neighbour = 0;
+                        if (y_neighbour < 0)
+                            y_neighbour = gridSizeY - 1;
+                        if (y_neighbour >= gridSizeY)
+                            y_neighbour = 0;
+
+                        if (grid[x_neighbour][y_neighbour].isAlive)
+                            aliveNeighbours += 1;
+                    }
+                }
+
+                // Rules:
+                // 1) Any live cell with fewer than two live neighbours dies
+                // 2) Any live cell with two or three live neighbours lives on to the next generation
+                // 3) Any live cell with more than three live neighbours dies
+                // 4) Any dead cell with exactly three live neighbours becomes a live cell
+
+                if (grid[x][y].isAlive)
+                {
+                    if (aliveNeighbours < 2)
+                        grid[x][y].nextState = false;
+                    if (aliveNeighbours > 3)
+                        grid[x][y].nextState = false;
+                }
+                else
+                {
+                    if (aliveNeighbours == 3)
+                        grid[x][y].nextState = true;
+                }
+            }
+        }
+
+        // apply changes
+        for (int y = 0; y < gridSizeY; y++)
+        {
+            for (int x = 0; x < gridSizeX; x++)
+            {
+                grid[x][y].isAlive = grid[x][y].nextState;
+            }
+        }
+    }
+    return 0;
+}
+
+void DrawLine(int l)
+{
+
+    for (int i = 0; i < l; i++)
+    {
+        cout << "-";
+    }
+    cout << '\n';
+}
